c621fa6100940be53dd3d66f0ea3664af9160b8c
roopamgarg <roopamg777@gmail.com>
2020-04-04 10:10:18 +0000

    added some new problems

--- roopam/bin.js
+++ roopam/bin.js
@@ -25,3 +25,4 @@ console.log(searchInsert([1,3,5,6],2) === 1)
 console.log(searchInsert([1,3],2) === 1)
 console.log(searchInsert([1,3,5,6],0) === 0)
 console.log(searchInsert([1,3,5,6],7) === 4)
+//comment
